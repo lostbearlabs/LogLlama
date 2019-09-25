498ff7288cc4055e8e4e4b64a0bd80674362ea78
Eric Johnson <eric.johnson@code42.com>
2019-09-25 15:17:05 -0500

    fix error in state after run fails, preventing subsequent runs

--- Script/ScriptEngine.swift
+++ Script/ScriptEngine.swift
@@ -14,34 +14,38 @@ class ScriptEngine {
         let (rc, cmds) = parser.parse(script: script)
         if( !rc ) {
             self.callback.scriptUpdate(text: "PARSING FAILED")
+            self.callback.scriptDone(logLines: [])
             return
         }
         
         if cmds.count==0 {
             self.callback.scriptUpdate(text: "NO COMMANDS IN SCRIPT")
+            self.callback.scriptDone(logLines: [])
             return
         }
-
+        
         var runState = RunState()
         
         for cmd in cmds {
             if( !cmd.validate()) {
+                self.callback.scriptDone(logLines: [])
                 return
             }
         }
-
+        
         var logLines : [LogLine] = []
         for cmd in cmds {
             let start = DispatchTime.now()
             
             if( !cmd.run(logLines: &logLines, runState: &runState)) {
+                self.callback.scriptDone(logLines: logLines)
                 return
             }
             
             let end = DispatchTime.now()
             let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
             let timeInterval = round( 1_000 * Double(nanoTime) / 1_000_000_000) / 1000
-
+            
             self.callback.scriptUpdate(text: "... (\(timeInterval) seconds)")
         }
         
