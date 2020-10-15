9632efba771641dae99efae557bb76eacde2bd61
Eric Johnson <eric.johnson@code42.com>
2020-10-15 10:50:14 -0500

    add unit test AddFieldCommandTest

---
+++ LogLlamaTests/AddFieldCommandTest.swift
@@ -0,0 +1,21 @@
+import XCTest
+import Hamcrest
+@testable import LogLlama
+
+class AddFieldCommandTest: XCTestCase {
+    func test_run_linkingFieldExists_copiesDependentField() {
+        // arrange
+        let ctx = CommandTestContext()
+        let sut = AddFieldCommand(callback: ctx, fieldToAdd: "dependentField", fieldToMatch: "linkField")
+        ctx.addLines(numLines: 2);
+        ctx.lines[0].namedFieldValues.updateValue("linkValue", forKey: "linkField");
+        ctx.lines[1].namedFieldValues.updateValue("linkValue", forKey: "linkField");
+        ctx.lines[1].namedFieldValues.updateValue("dependentValue", forKey: "dependentField");
+
+        // act
+        _ = sut.run(logLines: &ctx.lines, runState: &ctx.runState)
+
+        // assert
+        assertThat(ctx.lines[0].namedFieldValues["dependentField"], equalTo("dependentValue"))
+    }
+}
