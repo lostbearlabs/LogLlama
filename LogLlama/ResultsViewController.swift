06142cefe4ef81da03788ca1374d49f1741a60c5
Eric Johnson <eric3517@icloud.com>
2024-07-31 16:59:09 -0500

    only show message field section if there are some fields

--- LogLlama/ResultsViewController.swift
+++ LogLlama/ResultsViewController.swift
@@ -4,15 +4,15 @@ import Cocoa
  This controller manages the panel that shows (filtered, colored) log lines produced by script execution.
  */
 class ResultsViewController: NSViewController {
-
+    
     @IBOutlet weak var tableView: NSTableView!
     @IBOutlet weak var textCell: NSTextFieldCell!
     @IBOutlet weak var textColumn: NSTableColumn!
     @IBOutlet weak var searchField: NSSearchField!
-
+    
     var logLines : [LogLine] = []
     var longestLineLength = 1
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,15 +24,15 @@ class ResultsViewController: NSViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(onFontSizeUpdated(_:)), name: .FontSizeUpdated, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(onShowLineDetailClicked(_:)), name: .ShowLineDetailClicked, object: nil)
     }
-
+    
     @objc private func onShowLineDetailClicked(_ notification: Notification) {
         self.onDoubleClick(self)
     }
-
+    
     @objc private func onLogLinesUpdated(_ notification: Notification) {
-
+        
         self.logLines = []
-
+        
         if let update = notification.object as? LogLinesUpdate
         {
             self.longestLineLength = 1
@@ -46,7 +46,7 @@ class ResultsViewController: NSViewController {
         
         self.resizeColumn()
     }
-
+    
     func resizeColumn() {
         // It would be inefficient to go through every line, apply the current font, and then check
         // its width to find the maximum.  Instead, just use a wide character (W) and multiply its
@@ -75,7 +75,7 @@ class ResultsViewController: NSViewController {
     
     @IBAction func onDoubleClick(_ sender: Any) {
         let viewController = self.storyboard?.instantiateController(withIdentifier: "LogDetail") as! LineDetailViewController
-
+        
         let text = self.getSelectedText(includeFields: true);
         if( text.count > 0 ) {
             viewController.setText(text: text)
@@ -83,16 +83,16 @@ class ResultsViewController: NSViewController {
             self.presentAsModalWindow(viewController)
         }
     }
-
+    
     @objc func copy(_ sender: AnyObject?){
-
+        
         let text = self.getSelectedText(includeFields: false)
-
+        
         let pasteBoard = NSPasteboard.general
         pasteBoard.clearContents()
         pasteBoard.setString(text, forType:NSPasteboard.PasteboardType.string)
     }
-
+    
     func getSelectedText(includeFields: Bool) -> String {
         var text = ""
         for (_,idx) in (tableView?.selectedRowIndexes.enumerated())! {
@@ -100,17 +100,17 @@ class ResultsViewController: NSViewController {
                 text = text + "\n"
             }
             text = text + self.logLines[idx].text
-
-            if( includeFields ) {
-                text = text + "\n-----------  \n"
+            
+            if( includeFields && !self.logLines[idx].namedFieldValues.isEmpty ) {
+                text = text + "\n\n-----------  \n"
                 text = text + self.formatFieldListForLine(namedFieldValues: self.logLines[idx].namedFieldValues)
                 text = text + "-----------  \n"
             }
         }
-
+        
         return text
     }
-
+    
     func formatFieldListForLine(namedFieldValues: [String:String]) -> String {
         var text = ""
         let keys = namedFieldValues.keys.sorted()
@@ -120,24 +120,24 @@ class ResultsViewController: NSViewController {
         }
         return text
     }
-
+    
     func getSelectedRow() -> Int {
         for (_,idx) in (tableView?.selectedRowIndexes.enumerated())! {
             return idx
         }
         return -1
     }
-
+    
     @IBAction func onSearchTextChanged(_ sender: Any) {
         let sel = self.getSelectedRow() + 1
         self.doFind(searchText: self.searchField.stringValue, startPos: sel, incr: 1)
     }
-
+    
     @IBAction func onClickedNext(_ sender: Any) {
         let sel = self.getSelectedRow() + 1
         self.doFind(searchText: self.searchField.stringValue, startPos: sel, incr: 1)
     }
-
+    
     @IBAction func onClickedPrev(_ sender: Any) {
         var sel = getSelectedRow()
         if( sel<0 ) {
@@ -145,13 +145,13 @@ class ResultsViewController: NSViewController {
         }
         self.doFind(searchText: self.searchField.stringValue, startPos: sel - 1, incr: -1)
     }
-
-
+    
+    
     func doFind(searchText: String, startPos: Int, incr: Int) {
         if( searchText == "" ) {
             return
         }
-
+        
         for i in 0...self.logLines.count {
             let x = (startPos + i*incr + self.logLines.count) % self.logLines.count
             if self.logLines[x].text.lowercased().contains(searchText.lowercased()) {
@@ -163,7 +163,7 @@ class ResultsViewController: NSViewController {
         }
     }
     
-
+    
 }
 
 extension ResultsViewController: NSTableViewDataSource {
